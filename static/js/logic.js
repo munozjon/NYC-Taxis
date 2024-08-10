1309a2aaf468b04d8157b07b946a4551b67ec1dd
munozjon <munozjon98@gmail.com>
2024-08-10 10:32:36 -0400

    Testing endpoints with JavaScript, importing json file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,64 @@
 
+// Initialize the map
+mapboxgl.accessToken = 'pk.eyJ1IjoiY21kdXJhbiIsImEiOiJjbHpseXRldXQwODFjMm1vYzRuNnZhdjg0In0.09gQaWtNV7hLGC67Fd7R4w';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
+    zoom: 11,
+    center: [-73.96, 40.78] // starting position [lng, lat]
+});
+
+// TESTING
+// geoJson = 'https://data.cityofnewyork.us/api/geospatial/d3c5-ddgc?method=export&format=GeoJSON'
+// json = 'http://127.0.0.1:5000/coordinates.json'
+// fetch(json)
+//     .then((response) => console.log(response))
+//     .then((json) => console.log(json));
+
+// Add map controls
+map.addControl(new mapboxgl.NavigationControl());
+map.scrollZoom.disable();
+
+map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+
+    map.addSource('taxi-zones', {
+        type: 'geojson',
+        data: 'https://data.cityofnewyork.us/api/geospatial/d3c5-ddgc?method=export&format=GeoJSON'
+    });
+
+    // // Border around the polygons
+    map.addLayer({
+        'id': 'taxi-zones-outline',
+        'type': 'line',
+        'source': 'taxi-zones',
+        'layout': {},
+        'paint': {
+            'line-color': '#000000',
+            'line-width': 1,
+            'fill-color': {
+                property: 'frequency',
+                stops: [[2, '#fff'], [5, '#f00']]
+                }
+        }
+    });
+
+
+
+
+});
+
+
 // Initial page load function
 function init() {
     
-    const room_url = "http://127.0.0.1:5000/seaborn";
+    const room_url = "http://127.0.0.1:5000/heatmap";
     fetch(room_url)
         .then(function(response) {
             return response.text();
         }).then(function(data) {
-            document.getElementById("pie")
+            document.getElementById("heatmap")
             .innerHTML += data;
         });    
     
@@ -17,7 +68,7 @@ function init() {
         .then(function(response) {
             return response.text();
         }).then(function(data) {
-            document.getElementById("bar")
+            document.getElementById("histogram")
             .innerHTML += data;
         });    
 
@@ -27,11 +78,11 @@ function init() {
         .then(function(response) {
             return response.text();
         }).then(function(data) {
-            document.getElementById("box")
+            document.getElementById("bar")
             .innerHTML += data;
         });    
     
     };
 
-    init();
+init();
     
--- static/py/app.py
+++ static/py/app.py
@@ -1,10 +1,13 @@
 # Import dependencies
 import pandas as pd
 # from json import loads, dumps
-from flask import Flask, render_template_string #, send_file, Response
+from flask import Flask, render_template_string, jsonify #, send_file, Response
 from flask_cors import CORS
+
+# Set the backend to 'Agg
 import matplotlib
 matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 # from matplotlib.figure import Figure
 # from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -14,28 +17,74 @@ import numpy as np
 import base64
 
 # Older df, used for testing
-df = pd.read_csv('Projects/test/NYC-Taxis/static/data/uber_nyc_2023_1.csv')
-df_ints = df.drop(columns=['hvfhs_license_num', 'request_datetime', 'date', 'PUBorough', 'PUZone', 'DOBorough', 'DOZone'])
+df = pd.read_csv('static/data/uber_nyc_2023_1.csv')
+# df_ints = df.drop(columns=['Unnamed: 0', 'hvfhs_license_num', 'request_datetime', 'date', 'PUBorough', 'PUZone', 'DOBorough', 'DOZone'])
 
 # Actual DFs to use
-# df_1 = pd.read_csv('Projects/test/NYC-Taxis/static/data/uber_nyc_2023_1.csv')
-# df_2 = pd.read_csv('Projects/test/NYC-Taxis/static/data/uber_nyc_2023_2.csv')
-# df_3 = pd.read_csv('Projects/test/NYC-Taxis/static/data/uber_nyc_2023_3.csv')
+# df_1 = pd.read_csv('../data/uber_nyc_2023_1.csv')
+# df_2 = pd.read_csv('../data/uber_nyc_2023_2.csv')
+# df_3 = pd.read_csv('../data/uber_nyc_2023_3.csv')
 # new_df = pd.concat([df_1, df_2, df_3])
 # new_df_ints = new_df.drop(columns=['hvfhs_license_num', 'request_datetime', 'date', 'PUBorough', 'PUZone', 'DOBorough', 'DOZone'])
 
 
 
+# TESTING
+# def get_coords():
+    # # Import coordinates DataFrame
+    # coordinates_df = pd.read_csv('static/data/taxi_zone_lookup_coordinates.csv')
+
+
+#     # Merge df with coordinates_df on DOLocationID
+#     df_merged_DO = pd.merge(df, coordinates_df, left_on='DOLocationID', right_on='LocationID', suffixes=('', '_DO'))
+
+#     # Merge the resulting dataframe with coordinates_df again on PULocationID
+#     df_merged_both = pd.merge(df_merged_DO, coordinates_df, left_on='PULocationID', right_on='LocationID', suffixes=('_DO', '_PU'))
+
+#     # Drop the additional 'LocationID' columns from the merges
+#     df_merged_both = df_merged_both.drop(columns=['LocationID_DO', 'LocationID_PU', 'service_zone_DO', 'service_zone_PU', 
+#                                                 'Borough_PU', 'Zone_PU','Borough_DO', 'Zone_DO'
+#                                                 ])
+
+#     # Bin the DOLocationID's into 10 columns based on frequency
+#     DO_qc = pd.qcut(df_merged_both['DOLocationID'].value_counts(), q=10, precision=0)
+#     df_bins_DO = pd.merge(df_merged_both, DO_qc, left_on='DOLocationID', right_index=True)
+#     df_bins_DO = df_bins_DO.rename(columns={'count': 'DO_bins'})
+
+#     # Bin the PULocationID's into 10 columns based on frequency
+#     PU_qc = pd.qcut(df_merged_both['PULocationID'].value_counts(), q=10, precision=0)
+#     df_coord_bins_final = pd.merge(df_bins_DO, PU_qc, left_on='PULocationID', right_index=True)
+#     df_coord_bins_final = df_coord_bins_final.rename(columns={'count': 'PU_bins'})
+
+#     df_coord_bins_final = df_coord_bins_final[['DOLocationID', 'PULocationID', 'DO_bins', 'PU_bins']]
+
+#     df_coord_bins_final = df_coord_bins_final.astype({'DO_bins': 'object', 'PU_bins': 'object'})
+
+#     return df_coord_bins_final.to_json(orient="records")
+
+
+
 # Create heatmap with Seaborn
 def create_heatmap():
+
+    # Reduce to only int or float columns
+    df_ints = df.drop(columns=['Unnamed: 0', 'hvfhs_license_num', 'request_datetime', 'date', 'PUBorough', 'PUZone', 'DOBorough', 'DOZone'])
+
+    # Set the style of the plot
     sns.set_style(style="darkgrid")
-    fig, ax = plt.subplots()
+    
+    # Initialize subplot for this plot
+    fig, ax = plt.subplots(figsize=(9,9))
+
+    # Create the heatmap
     sns.heatmap(round(df_ints.corr(),2), annot=True, cmap='PuBuGn', ax=ax).set_title('Related Features')
 
+    # Add a buffer in the memory before saving the figure and then returning a base64-encoded string
     buf = BytesIO()
     fig.savefig(buf, format='png')
     buf.seek(0)
     return base64.b64encode(buf.getvalue()).decode('utf-8')
+
 
 # Create histogram with Seaborn
 def create_histogram():
@@ -49,7 +98,7 @@ def create_histogram():
     return base64.b64encode(buf.getvalue()).decode('utf-8')
 
 
-# Helper function to create a Matplotlib plot
+# Helper function to create a Matplotlib plot (EXAMPLE DATA)
 def create_matplotlib_plot():
     fig, ax = plt.subplots()
     ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
@@ -61,14 +110,16 @@ def create_matplotlib_plot():
     return base64.b64encode(buf.getvalue()).decode('utf-8')
 
 
+
+
 # Establish Flask connection
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/seaborn')
+@app.route('/heatmap')
 def seaborn_plot():
     plot_url = create_heatmap()
-    html = f'<img src="data:image/png;base64,{plot_url}" alt="Seaborn Plot">'
+    html = f'<img src="data:image/png;base64,{plot_url}" alt="Heatmap">'
     return render_template_string(html)
 
 @app.route('/histogram')
@@ -83,6 +134,12 @@ def matplotlib_plot():
     plot_url = create_matplotlib_plot()
     html = f'<img src="data:image/png;base64,{plot_url}" alt="Matplotlib Plot">'
     return render_template_string(html)
+
+
+# # Set endpoint for JSON:
+# @app.route('/coordinates.json')
+# def get_coordinates(): 
+#     return jsonify(get_coords())
 
 
 if __name__ == '__main__':
